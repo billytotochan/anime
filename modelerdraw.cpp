0c2f0817a31d730b4a2ef6a4eb156c5472fdc6d9
Billy <nillytotochan@gmail.com>
2015-11-27 15:18:43 +0800

    add pi

--- modelerdraw.cpp
+++ modelerdraw.cpp
@@ -3,6 +3,9 @@
 #include <GL/glu.h>
 #include <cstdio>
 
+#ifndef M_PI
+#define M_PI 3.141592653589793238462643383279502f
+#endif 
 // ********************************************************
 // Support functions from previous version of modeler
 // ********************************************************
